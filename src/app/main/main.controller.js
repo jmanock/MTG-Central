18c5687071c2e4a9b154ba7dd85f3dd10f1de8c7
Jon Manock <jonmanock@gmail.com>
2014-12-01 00:38:36 -0500

    wip making the list

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -1,17 +1,93 @@
 'use strict';
 
- angular.module('mtgCentral', [])
-.controller('MainCtrl', function ($scope, $http){
-  $http.get('sets.json').success(function(data) {
-    $scope.sets = data;
-    $scope.selection = [];
-    $scope.toggle = function(idk){
-      var pos = $scope.selection.indexOf(idk);
-      if(pos === -1){
-        $scope.selection.push(idk);
-      }else{
-        $scope.selection.splice(pos, 1);
+//  var demo = angular.module('mtgCentral', [])
+// .controller('MainCtrl', function ($scope, $http){
+//   $http.get('sets.json').success(function(data) {
+//     $scope.sets = data;
+//     $scope.deselectCard = function(card){
+//       var index = $scope.selectedCards.indexOf(card);
+//       if(index >= 0){
+//         $scope.selectedCards.splice(index, 1);
+//       }
+//     };
+//     $scope.selectCard = function(card){
+//       $scope.selectedCards.push(card);
+//     };
+//     demo.controller(
+//       'ItemController',function($scope){
+//         $scope.toggleSelection = function(){
+//           $scope.isSelected = ! $scope.isSelected;
+//           if($scope.isSelected){
+//             $scope.selectCard($scope.card);
+//           }else{
+//             $scope.deselectCard($scope.card);
+//           }
+//         };
+//       }
+//     );
+//   });
+// });
+var demo = angular.module( 'Demo', [] );
+
+demo.controller(
+  'DemoController',
+  function( $scope, $http ) {
+    $http.get('sets.json').success(function(data){
+      $scope.sets = data;
+    $scope.deselectCard = function( card ) {
+      var index = $scope.selectedCards.indexOf( card );
+
+      if ( index >= 0 ) {
+
+        $scope.selectedCards.splice( index, 1 );
+
       }
+
     };
-  });
+    $scope.selectCard = function( card ) {
+
+      $scope.selectedCards.push( card );
+
+    };
+    $scope.selectedCards = [];
+
+
+  }
+);
+
 });
+
+demo.controller(
+  'ItemController',
+  function( $scope ) {
+
+    $scope.deactivate = function() {
+
+      if ( $scope.isSelected ) {
+
+        return;
+
+      }
+
+    };
+
+    $scope.toggleSelection = function() {
+
+      $scope.isSelected = ! $scope.isSelected;
+
+      if ( $scope.isSelected ) {
+
+        $scope.selectCard( $scope.card );
+
+      } else {
+
+        $scope.deselectCard( $scope.card );
+
+      }
+
+    };
+    $scope.isSelected = false;
+
+
+  }
+);
