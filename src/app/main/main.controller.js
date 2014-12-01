3042630f8b9ec71dfda72172eed2776305a1aa60
Jon Manock <jonmanock@gmail.com>
2014-12-01 14:53:39 -0500

    now working

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -1,93 +1,18 @@
 'use strict';
 
-//  var demo = angular.module('mtgCentral', [])
-// .controller('MainCtrl', function ($scope, $http){
-//   $http.get('sets.json').success(function(data) {
-//     $scope.sets = data;
-//     $scope.deselectCard = function(card){
-//       var index = $scope.selectedCards.indexOf(card);
-//       if(index >= 0){
-//         $scope.selectedCards.splice(index, 1);
-//       }
-//     };
-//     $scope.selectCard = function(card){
-//       $scope.selectedCards.push(card);
-//     };
-//     demo.controller(
-//       'ItemController',function($scope){
-//         $scope.toggleSelection = function(){
-//           $scope.isSelected = ! $scope.isSelected;
-//           if($scope.isSelected){
-//             $scope.selectCard($scope.card);
-//           }else{
-//             $scope.deselectCard($scope.card);
-//           }
-//         };
-//       }
-//     );
-//   });
-// });
-var demo = angular.module( 'Demo', [] );
-
-demo.controller(
-  'DemoController',
-  function( $scope, $http ) {
-    $http.get('sets.json').success(function(data){
-      $scope.sets = data;
-    $scope.deselectCard = function( card ) {
-      var index = $scope.selectedCards.indexOf( card );
-
-      if ( index >= 0 ) {
-
-        $scope.selectedCards.splice( index, 1 );
-
+angular.module('mtgCentral', [])
+.controller('MainCtrl', function ($http){
+  var self = this;
+  $http.get('sets.json').success(function(data) {
+    self.sets = data;
+    self.selection = [];
+    self.toggle = function(s){
+      var pos = self.selection.indexOf(s);
+      if(pos === -1){
+        this.selection.push(s);
+      }else{
+        this.selection.splice(pos, 1);
       }
-
     };
-    $scope.selectCard = function( card ) {
-
-      $scope.selectedCards.push( card );
-
-    };
-    $scope.selectedCards = [];
-
-
-  }
-);
-
+  });
 });
-
-demo.controller(
-  'ItemController',
-  function( $scope ) {
-
-    $scope.deactivate = function() {
-
-      if ( $scope.isSelected ) {
-
-        return;
-
-      }
-
-    };
-
-    $scope.toggleSelection = function() {
-
-      $scope.isSelected = ! $scope.isSelected;
-
-      if ( $scope.isSelected ) {
-
-        $scope.selectCard( $scope.card );
-
-      } else {
-
-        $scope.deselectCard( $scope.card );
-
-      }
-
-    };
-    $scope.isSelected = false;
-
-
-  }
-);
