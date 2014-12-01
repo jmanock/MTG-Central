77e994cdcb40cfb6757775924fde7cd552b0693d
Jon Manock <jonmanock@gmail.com>
2014-11-30 20:49:48 -0500

    clicking on a card puts it into a list kind of

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -1,13 +1,17 @@
 'use strict';
 
-var mtgCentral = angular.module('mtgCentral', []);
-mtgCentral.controller('MainCtrl', function ($scope, $http){
+ angular.module('mtgCentral', [])
+.controller('MainCtrl', function ($scope, $http){
   $http.get('sets.json').success(function(data) {
     $scope.sets = data;
-    $scope.addName = function(){
-      $scope.names.push($scope.enteredName);
-      $scope.enteredName = '';
+    $scope.selection = [];
+    $scope.toggle = function(idk){
+      var pos = $scope.selection.indexOf(idk);
+      if(pos === -1){
+        $scope.selection.push(idk);
+      }else{
+        $scope.selection.splice(pos, 1);
+      }
     };
-    
   });
 });
