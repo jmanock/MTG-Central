ca394df031da62ebc48f360ef2c17649e790a809
Jon Manock <jonmanock@gmail.com>
2014-12-02 01:02:49 -0500

    firebase call works

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -1,18 +1,24 @@
 'use strict';
 
-angular.module('mtgCentral', [])
-.controller('MainCtrl', function ($http){
-  var self = this;
-  $http.get('sets.json').success(function(data) {
-    self.sets = data;
-    self.selection = [];
-    self.toggle = function(s){
-      var pos = self.selection.indexOf(s);
-      if(pos === -1){
-        this.selection.push(s);
-      }else{
-        this.selection.splice(pos, 1);
-      }
-    };
-  });
+angular.module('mtgCentral', ['firebase'])
+.controller('MainCtrl', function ($scope, $firebase){
+  var ref = new Firebase('https://mtg-central.firebaseio.com/');
+  var fb = $firebase(ref);
+  var syncObject = fb.$asObject();
+  syncObject.$bindTo($scope, 'sets');
+
 });
+//   $http.get('')
+//   .success(function(data) {
+//     self.sets = data;
+//     self.selection = [];
+//     self.toggle = function(s){
+//       var pos = self.selection.indexOf(s);
+//       if(pos === -1){
+//         this.selection.push(s);
+//       }else{
+//         this.selection.splice(pos, 1);
+//       }
+//     };
+//   });
+// });
