7478cb10b991fb13480e4a4e68014c7e4204ae2f
Alex Soper <alexsoper29@gmail.com>
2014-12-03 15:04:53 -0500

    Search function added and main updated

--- src/app/search/search.js
+++ src/app/search/search.js
@@ -1,12 +1,13 @@
 'use strict';
-var app = angular.module('mtgCentral', ['firebase']);
-app.factory('cards', ['$firebase',
-function($firebase){
-  var ref = new Firebase('https://mtg-central.firebaseio.com/cards')
-  .limitToFirst(500);
-  return $firebase(ref).$asArray();
-}]);
-app.controller('SearchCtrl', ['$scope', 'cards',
-function($scope, cards){
-  $scope.sets = cards;
-}]);
+angular.module('mtgCentral')
+
+  .factory('cards', ['$firebase', function($firebase){
+    var ref = new Firebase('https://mtg-central.firebaseio.com/cards')
+    .limitToFirst(500);
+    return $firebase(ref).$asArray();
+  }])
+
+  .controller('SearchCtrl', ['cards',
+  function(cards){
+    this.sets = cards;
+  }]);
