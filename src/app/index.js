6a89a198a54d61b63ea9df975a7c8660ba05df14
Jon Manock <jonmanock@gmail.com>
2014-12-02 19:28:46 -0500

    using facebook auth

--- src/app/index.js
+++ src/app/index.js
@@ -12,3 +12,27 @@ angular.module('mtgCentral', ['ngAnimate', 'ngCookies', 'restangular', 'ngRoute'
       });
   })
 ;
+// Facebook auth demo
+var ref = new Firebase('https://mtg-central.firebaseio.com/');
+
+console.debug(ref);
+
+ // Only do this for login...
+ref.authWithOAuthPopup('facebook', function() {
+console.debug(arguments);
+});
+
+
+/** // Only do this for logout...
+ref.unauth();
+*/
+
+var users = new Firebase('https://mtg-central.firebaseio.com/users');
+
+var authdUser = ref.getAuth();
+users.child(authdUser.uid).set({
+  uid: authdUser.uid,
+  facebook: authdUser.facebook,
+  fullName: authdUser.facebook.displayName,
+  avatarUrl: authdUser.facebook.cachedUserProfile.picture.data.url,
+});
--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -1,13 +1,21 @@
 'use strict';
 
+// FireBase call
+
 angular.module('mtgCentral', ['firebase'])
 .controller('MainCtrl', function ($scope, $firebase){
-  var ref = new Firebase('https://mtg-central.firebaseio.com/');
+  var ref = new Firebase('https://mtg-central.firebaseio.com/cards')
+  .limitToFirst(100);
   var fb = $firebase(ref);
   var syncObject = fb.$asObject();
   syncObject.$bindTo($scope, 'sets');
 
 });
+
+
+//  Angular http get call
+
+
 //   $http.get('')
 //   .success(function(data) {
 //     self.sets = data;
