3be3ff284d6be0e0dfaee4b3d7b0ce0c00b1942e
Jon Manock <jonmanock@gmail.com>
2014-12-06 04:57:02 -0500

    working on typeahead

--- src/app/index.js
+++ src/app/index.js
@@ -3,45 +3,48 @@
 // Create our main mtgCentral module and injecting dependencies
 angular.module('mtgCentral', ['ngCookies', 'ngSanitize', 'restangular', 'ui.router', 'uiRouterStyles', 'firebase'])
 
-  // Declaring a constant variable for the base url
-  .constant('CONFIG', {
-    Firebase: {
-      baseUrl: 'https://mtg-central.firebaseio.com/'
+// Declaring a constant variable for the base url
+.constant('CONFIG', {
+  Firebase: {
+    baseUrl: 'https://mtg-central.firebaseio.com/'
+  }
+})
+
+// Configuring our routes
+.config(function ($stateProvider, $urlRouterProvider) {
+
+  $stateProvider
+
+  // Route to show our home page
+  .state('home', {
+    url: '/',
+    templateUrl: 'app/main/main.html',
+    controller: 'MainCtrl',
+    data: {
+      css: 'app/main/mainMTG.css'
     }
   })
 
-  // Configuring our routes
-  .config(function ($stateProvider, $urlRouterProvider) {
+  // Route to show the listings
+  .state('list', {
+    url: '/list',
+    templateUrl: 'app/listings/listings.html',
+    controller: 'ListCtrl',
+    data: {
+      css: 'app/listings/listings.css'
+    }
+  })
 
-    $stateProvider
-
-    // Route to show our home page
-    .state('home', {
-      url: '/',
-      templateUrl: 'app/main/main.html',
-      controller: 'MainCtrl',
-      data: {
-        css: 'app/main/mainMTG.css'
-      }
-    })
-
-    // Route to show the listings
-    .state('list', {
-      url: '/list',
-      templateUrl: 'app/listings/listings.html',
-      controller: 'ListCtrl',
-      data: {
-        css: 'app/listings/listings.css'
-      }
-    })
-
-    .state('search', {
-      url: '/search',
-      templateUrl: 'app/search/search.html',
-      controller: 'SearchCtrl',
-    });
-
-    // Catch all for routes
-    // Send users to default route (home)
-    $urlRouterProvider.otherwise('/');
+  .state('search', {
+    url: '/search',
+    templateUrl: 'app/search/search.html',
+    controller: 'SearchCtrl',
+    data:{
+      css:'app/search/search.css'
+    }
   });
+
+  // Catch all for routes
+  // Send users to default route (home)
+  $urlRouterProvider.otherwise('/');
+});
--- src/app/search/search.js
+++ src/app/search/search.js
@@ -1,13 +1,13 @@
 'use strict';
 angular.module('mtgCentral')
 
-  .factory('cards', ['$firebase', function($firebase){
-    var ref = new Firebase('https://mtg-central.firebaseio.com/cards')
-    .limitToFirst(500);
-    return $firebase(ref).$asArray();
-  }])
+.factory('cards', ['$firebase', function($firebase){
+  var ref = new Firebase('https://mtg-central.firebaseio.com/cards')
+  .limitToFirst(500);
+  return $firebase(ref).$asArray();
+}])
 
-  .controller('SearchCtrl', ['cards',
-  function(cards){
-    this.sets = cards;
-  }]);
+.controller('SearchCtrl', ['cards',
+function(cards){
+  this.sets = cards;
+}]);
