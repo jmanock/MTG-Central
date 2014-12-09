76cf29d1acdb0d069442e7fd77e14b422df3ce0f
Jon Manock <jonmanock@gmail.com>
2014-12-09 01:07:36 -0500

    debounce wip

--- src/app/search/search.js
+++ src/app/search/search.js
@@ -1,104 +1,110 @@
-
 'use strict';
 
 angular.module('mtgCentral')
 
-.factory('cards', ['$firebase', function($firebase){
-  var ref = new Firebase('https://mtg-central.firebaseio.com/cards')
-  //.limitToFirst(10000);
-  return $firebase(ref).$asArray();
-}])
-
-.factory('SearchSvc', ['$q', function($q){
+.factory('SearchSvc', function($q, $http){
   function SearchSvc() {}
   SearchSvc.prototype.searchByName = function(searchString) {
-    var deffered = $q.defer();
-    if (searchString.length >= 3) {
-      // Figure out the best way to add a delay
+    // TODO: Figure out the best way to add a delay
+    
+    var deferred = $q.defer();
 
-      $.ajax({
-        url: "http://api.mtgdb.info/search/" + searchString,
-        success: function(data) {
+    $http.get("http://api.mtgdb.info/search/" + searchString).success(function(data){
+      deferred.resolve(data);
+    });
 
-          deffered.resolve(data);
-        }
-      });
-    }
+    return deferred.promise;
 
-    return deffered.promise;
-
+    //return $http.get("http://api.mtgdb.info/search/" + searchString);
   };
   return (SearchSvc);
-}])
+})
 
-.controller('SearchCtrl', ['cards', 'SearchSvc', '$scope', function(cards, SearchSvc, $scope){
+
+.controller('SearchCtrl', ['SearchSvc', 'FirebaseUrl', '$scope', function(SearchSvc, FirebaseUrl, $scope){
   var self = this;
 
   var searchSvc = new SearchSvc();
 
-  this.sets = cards;
-  this.haves = [];
-  this.wants = [];
-  //var badSets = ['badSet'];
+  this.wantIds = [];
+  this.haveIds = [];
 
-  this.cards = [];
+  this.wants = [];
+  this.haves = [];
+  // TODO: var badSets = ['badSet'];
 
   this.searchByName = function(){
     self.cards = [];
-    searchSvc.searchByName($scope.searchForm).then(function(data){
-      data.forEach(function(card){
-        // Create an object of known online only sets and compare the set name
-        // To the object and remove those results
-        self.cards.push(card);
+    if ($scope.searchForm.length >= 3) {
+      searchSvc.searchByName($scope.searchForm).then(function(data){
+        data.forEach(function(card){
+          // TODO: Create an object of known online only sets and compare the set name
+          // To the object and remove those results
+          self.cards.push(card);
+        });
       });
-    });
+    }
+
   };
 
-// Add Item to either list
-this.addItem = function(index){
-  var list = self.haves;
-  // See what radio button has been checked
-  if($('#haveCheck').prop('checked')){
-  }else{
-     list = self.wants;
-  }
-  if(list.length === 0){
-    list.push(self.cards[index]);
-  }else{
-    var duplicate = false;
-    list.some(function(value){
-      var tempId = self.cards[index].id;
-      if(value.id === tempId){
-        duplicate = true;
-      }
-    });
-    if(duplicate === false){
+  this.addItem = function(index){
+    var list;
+    // See what check box has been checked
+    if ($('#haveCheck').prop('checked')){
+      list = self.haves;
+    } else {
+      list = self.wants;
+    }
+    if(list.length === 0){
       list.push(self.cards[index]);
+      if (list === self.wants){
+        self.wantIds.push(self.cards[index].id);
+      } else {
+        self.haveIds.push(self.cards[index].id);
+      }
+    }else{
+      var duplicate = false;
+      list.some(function(value){
+        var tempId = self.cards[index].id;
+        if(value.id === tempId){
+          duplicate = true;
+        }
+      });
+      if(duplicate === false){
+        list.push(self.cards[index]);
+        if (list === self.wants){
+          self.wantIds.push(self.cards[index].id);
+        } else {
+          self.haveIds.push(self.cards[index].id);
+        }
+      }
     }
-  }
+  };
 
-};
-
-// Remove Cards from `have` list
-this.removeItem = function(index){
-
-  var list = self.haves;
-
-  for(var i = 0; i < list.length; i++){
-    if (list[i].id == index){
-      list.splice(i,1);
+  this.removeItemHave = function(index){
+    for(var i = 0; i < self.haves.length; i++){
+      if (self.haves[i].id == index){
+        self.haves.splice(i,1);
+      }
     }
-  }
 
-};
+  };
 
-// Remove Cards from `want` list
-this.removeItemWant = function(index){
-  for(var i = 0; i <self.wants.length; i++){
-    if(self.wants[i].id == index){
-      self.wants.splice(i,1);
+  this.removeItemWant = function(index){
+    for(var i = 0; i < self.wants.length; i++){
+      if(self.wants[i].id == index){
+        self.wants.splice(i,1);
+      }
     }
-  }
-};
+  };
 
+  this.pushCards = function(id){
+    var user = FirebaseUrl.child('users').child(id);
+
+    // Update the authdUser's information in Firebase
+    user.update({
+      wants: self.wantIds,
+      haves: self.haveIds
+    });
+  };
 }]);
