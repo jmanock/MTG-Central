87169c9deafb38e178b90179d7a984b581bc4a0b
Jon Manock <jonmanock@gmail.com>
2014-12-10 15:56:39 -0500

    fixing bugs

--- src/app/search/search.js
+++ src/app/search/search.js
@@ -23,8 +23,8 @@ function(Auth, SearchSvc, FirebaseUrl, $scope, $firebase){
   });
 
   self.user.$loaded(function(){
-    self.haves = $firebase(FirebaseUrl.child('usercards').child(self.user.uid).child('have')).$asObject();
-    self.wants = $firebase(FirebaseUrl.child('usercards').child(self.user.uid).child('want')).$asObject();
+    self.haves = $firebase(FirebaseUrl.child('usercards').child(self.user.uid).child('haves')).$asObject();
+    self.wants = $firebase(FirebaseUrl.child('usercards').child(self.user.uid).child('wants')).$asObject();
   });
 
   this.searchByName = function(){
@@ -37,11 +37,12 @@ function(Auth, SearchSvc, FirebaseUrl, $scope, $firebase){
   };
 
   this.addItemHave = function(card){
-    var cardUsers = FirebaseUrl.child('cardusers').child(card.id).child('have').child(self.user.uid);
-    var userCards = FirebaseUrl.child('usercards').child(self.user.uid).child('have').child(card.id);
+    var cardUsers = FirebaseUrl.child('cardusers').child('haves').child(card.id).child(self.user.uid);
+    var userCards = FirebaseUrl.child('usercards').child(self.user.uid).child('haves').child(card.id);
 
     // Update the authdUser's information in Firebase
     cardUsers.update({
+      facebook: {uid: self.user.uid, fullName: self.user.fullName, avatarUrl: self.user.avatarUrl},
       cond: 'NM',
       qty: 1
     });
@@ -55,11 +56,12 @@ function(Auth, SearchSvc, FirebaseUrl, $scope, $firebase){
   };
 
   this.addItemWant = function(card){
-    var cardUsers = FirebaseUrl.child('cardusers').child(card.id).child('want').child(self.user.uid);
-    var userCards = FirebaseUrl.child('usercards').child(self.user.uid).child('want').child(card.id);
+    var cardUsers = FirebaseUrl.child('cardusers').child('wants').child(card.id).child(self.user.uid);
+    var userCards = FirebaseUrl.child('usercards').child(self.user.uid).child('wants').child(card.id);
 
     // Update the authdUser's information in Firebase
     cardUsers.update({
+      facebook: {uid: self.user.uid, fullName: self.user.fullName, avatarUrl: self.user.avatarUrl},
       cond: 'NM',
       qty: 1
     });
@@ -73,18 +75,19 @@ function(Auth, SearchSvc, FirebaseUrl, $scope, $firebase){
   };
 
 
-
   this.removeItemHave = function(id){
-    var cardUsers = FirebaseUrl.child('cardusers').child(id).child('have').child(self.user.uid);
-    var userCards = FirebaseUrl.child('usercards').child(self.user.uid).child('have').child(id);
+    var cardUsers = FirebaseUrl.child('cardusers').child('haves').child(id).child(self.user.uid);
+    var userCards = FirebaseUrl.child('usercards').child(self.user.uid).child('haves').child(id);
+
 
     cardUsers.remove();
     userCards.remove();
   };
 
   this.removeItemWant = function(id){
-    var cardUsers = FirebaseUrl.child('cardusers').child(id).child('want').child(self.user.uid);
-    var userCards = FirebaseUrl.child('usercards').child(self.user.uid).child('want').child(id);
+    var cardUsers = FirebaseUrl.child('cardusers').child('wants').child(id).child(self.user.uid);
+    var userCards = FirebaseUrl.child('usercards').child(self.user.uid).child('wants').child(id);
+
 
     cardUsers.remove();
     userCards.remove();
